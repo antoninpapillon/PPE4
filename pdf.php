d5b0dfdedb439fcb3861bddc356ffc4cd0190b82
antoninpapillon <antonin.papillon@bts-malraux.net>
2016-04-21 12:05:45 +0200

    Add bootsrap to pdf.php page

--- pdf.php
+++ pdf.php
@@ -1,7 +1,7 @@
 <?php
-
+    
 require('connexion.php');
-
+    
 if (isset($_SERVER['REQUEST_URI'])) {
     $generate = isset($_GET['make_pdf']);
     $nom = isset($_GET['nom']) ? $_GET['nom'] : '';
@@ -13,69 +13,33 @@ if (isset($_SERVER['REQUEST_URI'])) {
     $generate = true;
     $nom = '';
 }
-
-$mail = 'antonin.papillon@bts-malraux.net';
-
-//Prévention des éventuels problème lié aux retours à la ligne selon le serveur.
-if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail))
-{
-	$passage_ligne = "\r\n";
-}
-else
-{
-	$passage_ligne = "\n";
-}
-
-$message_txt = "TEST MICRO UN DEUX TROIS";
-
-$boundary = "-----=".md5(rand());
-
-$sujet = "Hey mon ami !";
-
-$header = "From: \"AUTO_PDF\"<antonin.papillon@bts-malraux.net>".$passage_ligne;
-$header .= "Reply-to: \"AUTO_PDF\" <antonin.papillon@bts-malraux.net>".$passage_ligne;
-$header .= "MIME-Version: 1.0".$passage_ligne;
-$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
-
-$message = $passage_ligne."--".$boundary.$passage_ligne;
-
-$message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
-$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-$message.= $passage_ligne.$message_txt.$passage_ligne;
-
-$message.= $passage_ligne."--".$boundary.$passage_ligne;
-
-$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-
-//mail($mail,$sujet,$message,$header);
-
+    
 //Préparation de la requête SQL
 $req = $bdd->prepare('INSERT INTO autorisation(nomSal, prenomSal, dateDebut, dateFin, motif) '
         . 'VALUES(:nomSal, :prenomSal, :dateDebut, :dateFin, :motif)') 
         or exit(print_r($bdd->errorInfo()));
-
-
-
+            
+            
+            
 //Ce qui va être affiché dans le PDF
 $pdf = 
 '<style>
     h1 {
         text-align: center;
     }
-    
+        
     .align1 {
         float: left;
     }
-    
+        
     .align2 {
         margin-left: 50px;
     }
-    
+        
     .signatureRes {
         margin-left: 400px;
     }
-    
+        
     .image {
         display: block;
         margin-left: 100px;
@@ -83,9 +47,9 @@ $pdf =
         width: 500px;
 	height: 200px;
     }
-    
+        
 </style>
-
+    
 <img src="assets/img/Fyctive.jpg" class="image"/>
 <br><br>
 <h1>Autorisation d\'absence</h1>
@@ -105,10 +69,11 @@ $pdf =
 <h2>Signatures</h2>
 <br>
 <p><span class="align1">Signature salarié : </span><span class="signatureRes">Signature resonsable : </span></p>
-
+    
 ';
-
+    
 if ($generate) {
+    
     //Exécution de la requête SQL
 $req->execute(array(
     'nomSal' => $nom,
@@ -117,6 +82,7 @@ $req->execute(array(
     'dateFin' => $dateFin,
     'motif' => $motif
 ));
+    
     ob_start();
 } 
 else {
@@ -126,26 +92,97 @@ else {
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
         <title>Exemple d'auto génération de PDF</title>
+            
+        <!-- Bootstrap Core CSS -->
+        <link rel="stylesheet" href="assets/bootsrap/css/bootstrap.min.css" type="text/css">
+            
+        <!-- Custom Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+            
+        <!-- Plugin CSS -->
+        <link rel="stylesheet" href="assets/bootsrap/css/animate.min.css" type="text/css">
+            
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="assets/bootsrap/css/creative.css" type="text/css">
     </head>
     <body>
 <?php
 }
 ?>
-        <form method="get" action="">
-            <input type="hidden" name="make_pdf" value="">
-            Ton nom : <input type="text" name="nom" value="">
-            Prénom : <input type="text" name="prenom" value="">
-            <br><br>
-            Date de début : <input type="text" name="dateDebut" value="">
-            Date de fin : <input type="text" name="dateFin" value="">
-            <br><br>
-            Motif : <input type="text" name="motif" value="">
-            <br><br>
-            <input type="submit" value="Generer le PDF" >
-        </form>
-    </body>
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    
+                    <form method="get" action="">
+                        <input type="hidden" name="make_pdf" value="">
+                        <h2 class="section-heading">Formulaire</h2>
+                        <hr class="light">
+ 
+                        <div class="col-lg-6 col-md-6 text-center">
+                            <p>Nom</p>
+                            <input type="text" class="form-control" id="nom" name="nom" /><p></br>
+                        </div>
+                        
+                        <div class="col-lg-6 col-md-6 text-center">
+                            <p>Prénom</p>
+                            <input type="text" class="form-control" id="prenom" name="prenom" /><p></br>
+                        </div>
+                        
+                        <div class="col-lg-6 col-md-6 text-center">
+                            <p class="text-faded">Date de début</p>
+                            <input type="text" class="form-control" id="dateDebut" name="dateDebut" /><p></br>
+                        </div>
+                        
+                        <div class="col-lg-6 col-md-6 text-center">
+                            <p class="text-faded">Date de fin</p>
+                            <input type="text" class="form-control" id="dateFin" name="dateFin"/><p></br>
+                        </div>
+                        
+                        <div class="col-lg-6 col-lg-offset-3 text-center">
+                            <p>Motif</p>
+                            <input type="text" class="form-control" id="motif" name="motif"/><p></br>
+                        </div>
+                        
+                        <div class="col-lg-6 col-lg-offset-3 text-center">
+                            <input type="submit" value="Generer le PDF" class="btn btn-default btn-xl">
+                        </div>
+                        
+                            
+<!--                        <input type="hidden" name="make_pdf" value="">
+                        Ton nom : <input type="text" name="nom" value="">
+                        Prénom : <input type="text" name="prenom" value="">
+                        <br><br>
+                        Date de début : <input type="text" name="dateDebut" value="">
+                        Date de fin : <input type="text" name="dateFin" value="">
+                        <br><br>
+                        Motif : <input type="text" name="motif" value="">
+                        <br><br>
+                        <input type="submit" value="Generer le PDF" >-->
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+        
+    <script src="assets/bootsrap/js/jquery.js"></script>
+        
+    <!-- Bootstrap Core JavaScript -->
+    <script src="assets/bootsrap/js/bootstrap.min.js"></script>
+        
+    <!-- Plugin JavaScript -->
+    <script src="assets/bootsrap/js/jquery.easing.min.js"></script>
+    <script src="assets/bootsrap/js/jquery.fittext.js"></script>
+    <script src="assets/bootsrap/js/wow.min.js"></script>
+        
+    <!-- Custom Theme JavaScript -->
+    <script src="assets/bootsrap/js/creative.js"></script>
+        
+</body>
 </html>
-
+    
 <?php
     if ($generate) {
         $content = ob_get_clean();
