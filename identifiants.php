d8989c4a5a25809212ffcdabe5fdf515def67893
antoninpapillon <antonin.papillon@bts-malraux.net>
2016-05-30 10:21:42 +0200

    Ajout de la page identifiants.php

---
+++ identifiants.php
@@ -0,0 +1,6 @@
+<?php
+$user = "A remplacer";
+$mdp = "A remplacer";
+$dbname = 'mysql:dbname=a.papillon;host=172.16.99.3';
+//$dbname = 'mysql:host=bts.bts-malraux72.net;port=63330;dbname=a.papillon;';
+?>
